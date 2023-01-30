bb8a717aa232d6870c92b790eea6bdc7d275383a
Виктор Алтунин <altvik@yandex.ru>
2023-01-30 20:29:18 +0600

    compose

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = False
+DEBUG = False  #
 
 
 SECRET_KEY = os.getenv(
